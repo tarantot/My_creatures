7b5d71e903ef030b2484192a05f56f0d7d5d83cc
tarantot <60243020+tarantot@users.noreply.github.com>
2020-01-29 17:50:15 +0200

    Add files via upload

---
+++ homework1Jan30-1.py
@@ -0,0 +1,25 @@
+i = 1
+while i == 1:
+	a = float( input( "Введите первое число: "))
+	b = float( input( "Введите второе число: "))
+	c = float( input( "Введите третье число: "))
+	e = (a + c)
+	g = (b - c)
+
+if a == b:
+	print( "А если так?")
+	def z(e, f):
+	    if e == f:
+		    print( "Введите числа заново!" )
+		elif e > f:
+	    	return "e > f"
+		elif e < f:
+			return "e < f"		   	 	   	
+	print( z(e, f))
+
+elif a > b:
+	print( "Свершилось!")
+
+else:
+	print( "Да ну!")	
+	
