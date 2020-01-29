0eb0b1b2ca757c1dd24a36b6770920e30931e2ac
tarantot <60243020+tarantot@users.noreply.github.com>
2020-01-29 22:53:41 +0200

    Update homework1Jan30-1.py

--- homework1Jan30-1.py
+++ homework1Jan30-1.py
@@ -1,25 +1,16 @@
-i = 1
-while i == 1:
-	a = float( input( "Введите первое число: "))
-	b = float( input( "Введите второе число: "))
-	c = float( input( "Введите третье число: "))
-	e = (a + c)
-	g = (b - c)
+a = float( input( "Введите первое число: "))
+b = float( input( "Введите второе число: "))
+c = float( input( "Введите третье число: "))
 
 if a == b:
 	print( "А если так?")
-	def z(e, f):
-		if e == f:
-			print( "Введите числа заново!" )
-		elif e > f:
-      			return "e > f"
-		elif e < f:
-			return "e < f"		   	 	   	
-	print( z(e, f))
+	if a+c > b-c:
+		print( str(a+c) + ">" + str(b-c))
+	elif a+c < b-c:
+		print( str(a+c) + "<" + str(b-c))
 
 elif a > b:
 	print( "Свершилось!")
 
-else:
+elif b > a:
 	print( "Да ну!")	
-	
